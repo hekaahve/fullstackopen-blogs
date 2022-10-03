f8b00d61d45bbacbc8efe5283cdf40a6c6d214ef
Heini Ahven <heini.ahven@gmail.com>
2022-10-03 22:35:26 +0300

    refactoring made accordance with 4.2

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
+const app = require('./app')
 const http = require('http')
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-//TODO refactor whole thing according to 4.1 blogilista, step1. Installers done, put things in
-//separate files
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- models/blog.js
+++ models/blog.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
   const blogSchema = mongoose.Schema({
     title: String,
     author: String,
