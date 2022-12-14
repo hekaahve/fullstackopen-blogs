30bd537f15072148d3969b90459c88cf9b64dc7e
Heini Ahven <heini.ahven@gmail.com>
2022-12-14 20:55:12 +0200

    put and post added

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,32 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
-  module.exports = blogsRouter
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
+
+blogsRouter.post("/", async (request, response) => {
+  const blog = new Blog(request.body);
+
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
+blogsRouter.put("/:id", async (request, response) => {
+  const blog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+module.exports = blogsRouter;
