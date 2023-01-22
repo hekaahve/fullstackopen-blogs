01988673c5839e2fc3e1e0afc620e64ebf3746bb
Heini Ahven <heini.ahven@gmail.com>
2023-01-22 21:09:40 +0200

    password & username exeption handlers

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ usersRouter.get("/", async (request, response) => {
   response.json(users);
 });
 
-usersRouter.post("/", async (request, response) => {
+usersRouter.post("/", async (request, response, next) => {
   const { username, name, password } = request.body;
 
   const existingUser = await User.findOne({ username });
@@ -16,7 +16,16 @@ usersRouter.post("/", async (request, response) => {
       error: "username must be unique",
     });
   }
-
+  if (username.length < 3) {
+    return response.status(400).json({
+      error: "username length must be 3",
+    });
+  }
+  if (password.length < 3) {
+    return response.status(400).json({
+      error: "password length must be 3",
+    });
+  }
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
@@ -25,6 +34,7 @@ usersRouter.post("/", async (request, response) => {
     name,
     passwordHash,
   });
+
   const savedUser = await user.save();
 
   response.status(201).json(savedUser);
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  username: String,
+  username: { type: String, minlength: 3, required: true },
   name: String,
   passwordHash: String,
 });
